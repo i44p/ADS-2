7ae753f56550774d0bec3bb54e65889e5650d882
i44p <jsbbdjzjsjawk+@gmail.com>
2023-03-01 18:26:04 +0300

    implement pown

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,20 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
 
+#include <cstdint>
 
 double pown(double value, uint16_t n) {
-
+  double ret = value;
+  for (int i = 1; i < n; i++) ret *= ret;
+  return ret;
 }
 
-uint64_t fact(uint16_t n) {
+uint64_t fact(uint16_t n) { return 0.0; }
 
-}
+double calcItem(double x, uint16_t n) { return 0.0; }
 
-double calcItem(double x, uint16_t n) {
+double expn(double x, uint16_t count) { return 0.0; }
 
-}
+double sinn(double x, uint16_t count) { return 0.0; }
 
-double expn(double x, uint16_t count) {
-
-}
-
-double sinn(double x, uint16_t count) {
-
-}
-
-double cosn(double x, uint16_t count) {
-
-}
+double cosn(double x, uint16_t count) { return 0.0; }
