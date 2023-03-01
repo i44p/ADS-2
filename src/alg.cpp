3365f6d585b0ef868943e8da3500cfc8a3442a93
i44p <jsbbdjzjsjawk+@gmail.com>
2023-03-01 20:20:26 +0300

    implement expn

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,9 @@ uint64_t fact(uint16_t n) { return n > 1 ? n * fact(n - 1) : 1; }
 
 double calcItem(double x, uint16_t n) { return pown(x, n) / fact(n); }
 
-double expn(double x, uint16_t count) { return 0.0; }
+double expn(double x, uint16_t count) {
+  return count > 0 ? calcItem(x, count) + expn(x, count - 1) : 1;
+}
 
 double sinn(double x, uint16_t count) { return 0.0; }
 
